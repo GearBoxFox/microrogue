50fb81f05571a59bc2009ae212c9b7c7980211f9
GearBoxFox <favoringbasil55@gmail.com>
2024-07-03 11:33:50 -0400

    finish player script with animations and hit registration

--- player.cs
+++ player.cs
@@ -3,9 +3,13 @@ using System;
 
 public partial class player : Area2D
 {
+	// Hitbox signal
+	[Signal]
+	public delegate void HitEventHandler();
+
 	// Meber variables
 	[Export]
-	public int speed { get; set; } = 400; // How fast the player moves in pixels/second
+	public int Speed { get; set; } = 400; // How fast the player moves in pixels/second
 
 	public Vector2 ScreenSize; // Size of the game window
 
@@ -13,10 +17,78 @@ public partial class player : Area2D
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
+		Hide();
+	}
+
+	// Start the game
+	public void Start(Vector2 position) {
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var velocity = Vector2.Zero; // The player's movement vector.
+
+		// watch for each input
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+ 
+		// update animation based on movement
+		if (velocity.X != 0)
+		{
+			animatedSprite2D.Animation = "walk";
+			animatedSprite2D.FlipV = false;
+			animatedSprite2D.FlipH = velocity.X < 0;
+		} else if (velocity.Y != 0) {
+			animatedSprite2D.Animation = "up";
+			animatedSprite2D.FlipV = velocity.Y > 0;
+		}
+
+		// normalize velocity and animate sprite
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
+
+		// update position based on velocity
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+    		x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+    		y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+	}
+
+	private void OnBodyEntered(Node2D body) {
+		Hide(); // Player disappear after being hit
+		EmitSignal(SignalName.Hit);
+		// Disable collisions when safe
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
 	}
 }
