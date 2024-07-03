d0fc3c000b7b54648212ee225dca7b9931d4aabd
GearBoxFox <favoringbasil55@gmail.com>
2024-07-03 12:11:58 -0400

    finish timers and fix issue with player hit callback

--- main.cs
+++ main.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public partial class main : Node
 {
@@ -11,6 +10,7 @@ public partial class main : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		NewGame();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -44,5 +44,37 @@ public partial class main : Node
 	public void OnStartTimerTimeout() {
 		GetNode<Timer>("MobTimer").Start();
     	GetNode<Timer>("ScoreTimer").Start();
+	}
+
+	// Handle mob spawning
+	private void OnMobTimerTimeout()
+	{
+		// Note: Normally it is best to use explicit types rather than the `var`
+		// keyword. However, var is acceptable to use here because the types are
+		// obviously Mob and PathFollow2D, since they appear later on the line.
+
+		// Create a new instance of the Mob scene.
+		mob mob = MobScene.Instantiate<mob>();
+
+		// Choose a random location on Path2D.
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		// Set the mob's direction perpendicular to the path direction.
+		float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+		// Set the mob's position to a random location.
+		mob.Position = mobSpawnLocation.Position;
+
+		// Add some randomness to the direction.
+		direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+		mob.Rotation = direction;
+
+		// Choose the velocity.
+		var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+		mob.LinearVelocity = velocity.Rotated(direction);
+
+		// Spawn the mob by adding it to the Main scene.
+		AddChild(mob);
 	}
 }
