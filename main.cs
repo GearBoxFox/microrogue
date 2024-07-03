ef79fb79f63445a7381a8a17d816c62dafdc09ef
GearBoxFox <favoringbasil55@gmail.com>
2024-07-03 12:26:46 -0400

    add hud events

--- main.cs
+++ main.cs
@@ -10,7 +10,7 @@ public partial class main : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		NewGame();
+		return;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
