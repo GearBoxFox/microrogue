f7337df999da28f7db222f36e3515e3e33d704f4
GearBoxFox <favoringbasil55@gmail.com>
2024-07-15 13:44:59 -0400

    basic enemy script

--- scenes/characters/enemies/Enemy.cs
+++ scenes/characters/enemies/Enemy.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public partial class Enemy : Character
 {
@@ -12,10 +11,43 @@ public partial class Enemy : Character
 	{
 		Player = GetTree().CurrentScene.GetNode<CharacterBody2D>("Player");
 		PathTimer = GetNode<Timer>("PathTimer");
+		NavigationAgent = GetNode<NavigationAgent2D>("NavigationAgent");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}	
+
+	public void Chase()
+	{
+		if (!NavigationAgent.IsTargetReached())
+		{
+			var VecToNextPoint = NavigationAgent.GetNextPathPosition() - GlobalPosition;
+			MoveDirection = VecToNextPoint;
+
+			if (VecToNextPoint.X > 0.0 && AnimatedSprite.FlipH)
+			{
+				AnimatedSprite.FlipH = false;
+			} else if (VecToNextPoint.X < 0.0 && !AnimatedSprite.FlipH) {
+				AnimatedSprite.FlipH = true;
+			}
+		}
+	}
+
+	public void OnPathTimerTimeout()
+	{
+		if (Player != null)
+		{
+				GetPathToPlayer();
+		} else {
+			PathTimer.Stop();
+			MoveDirection = Vector2.Zero;
+		}
+	}
+
+	public void GetPathToPlayer() 
+	{
+		NavigationAgent.TargetPosition = Player.Position;
 	}
 }
