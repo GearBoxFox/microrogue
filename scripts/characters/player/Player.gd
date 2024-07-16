76ad9643ee125409004bc9a256236c5b1d991a49
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2024-07-16 15:06:31 -0400

    fix knockback amount and joystick not working

--- scripts/characters/player/Player.gd
+++ scripts/characters/player/Player.gd
@@ -14,15 +14,15 @@ func _process(_delta: float) -> void:
 	var mouse_direction: Vector2 = Vector2.ZERO
 	
 	if use_keyboard:
-		mouse_direction = (get_global_mouse_position() - global_position)
+		mouse_direction = (get_global_mouse_position() - global_position).normalized()
 	else:
-		mouse_direction = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down") 
+		mouse_direction = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down").normalized() 
 
 	# if no input, use last aim direction
 	if mouse_direction == Vector2.ZERO:
 		mouse_direction = prev_aim_position
 		
-	attack_indicator.position = mouse_direction.normalized() * indicator_distance
+	attack_indicator.position = mouse_direction * indicator_distance
 		
 	sword.rotation = mouse_direction.angle()
 	sword_hitbox.knockback_direction = mouse_direction
@@ -35,11 +35,11 @@ func _process(_delta: float) -> void:
 		sword_animation_player.play("attack")
 
 func _input(event):
-	if event is InputEventKey or InputEventMouse:
+	if (event is InputEventKey) or (event is InputEventMouse):
 		attack_indicator.hide()
 		use_keyboard = true
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	else:
+	elif (event is InputEventJoypadButton) or (event is InputEventJoypadMotion):
 		attack_indicator.show()
 		use_keyboard = false
 		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
