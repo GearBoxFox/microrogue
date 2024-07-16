a38b64d270aaf43ae0926055cd6d871c3586936e
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2024-07-16 13:52:04 -0400

    replace all C# scripts with GDScript scripts

---
+++ scripts/characters/enemies/Enemy.gd
@@ -0,0 +1,26 @@
+extends Character
+class_name Enemy
+
+@onready var player: Character = get_tree().current_scene.get_node("Player")
+@onready var path_timer: Timer = get_node("PathTimer")
+@onready var nav_agent: NavigationAgent2D = get_node("NavigationAgent")
+
+func get_input() -> void:
+	if not nav_agent.is_target_reached():
+		var vector_to_next_target: Vector2 = nav_agent.get_next_path_position() - global_position
+		move_direction = vector_to_next_target
+		
+		if vector_to_next_target.x > 0 and animated_sprite.flip_h:
+			animated_sprite.flip_h = false
+		elif vector_to_next_target.x < 0 and not animated_sprite.flip_h:
+			animated_sprite.flip_h = true
+			
+func _on_path_timer_timeout() -> void:
+	if is_instance_valid(player):
+		_get_path_to_player()
+	else:
+		path_timer.stop()
+		move_direction = Vector2.ZERO
+
+func _get_path_to_player() -> void:
+	nav_agent.target_position = player.global_position
---
+++ scripts/characters/player/Player.gd
@@ -0,0 +1,54 @@
+extends Character
+
+@export var indicator_distance: float = 75.0
+
+@onready var sword: Node2D = get_node("Sword")
+@onready var sword_animation_player: AnimationPlayer = sword.get_node("SwordAnimationPlayer")
+@onready var attack_indicator: Sprite2D = get_node("AttackIndicator")
+
+var use_keyboard: bool = true
+var prev_aim_position = Vector2.LEFT
+
+func _process(_delta: float) -> void:
+	var mouse_direction: Vector2 = Vector2.ZERO
+	
+	if use_keyboard:
+		mouse_direction = (get_global_mouse_position() - global_position)
+	else:
+		mouse_direction = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down") 
+
+	# if no input, use last aim direction
+	if mouse_direction == Vector2.ZERO:
+		mouse_direction = prev_aim_position
+		
+	if not sword_animation_player.is_playing():
+		sword.rotation = mouse_direction.angle()
+	
+	prev_aim_position = mouse_direction
+	
+	# handle attacking
+	if Input.is_action_pressed("attack") and not sword_animation_player.is_playing():
+		sword.show()
+		sword_animation_player.play("attack")
+
+func _input(event):
+	if event is InputEventKey or InputEventMouse:
+		attack_indicator.hide()
+		use_keyboard = true
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		attack_indicator.show()
+		use_keyboard = false
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
+func get_input() -> void:
+	move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+
+
+
+func _on_sword_animation_player_animation_finished(anim_name):
+	if anim_name == "attack":
+		sword.hide()
+		sword.get_node("Node").rotation = 0
+		
