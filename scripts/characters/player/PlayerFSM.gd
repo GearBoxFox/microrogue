8fea76db84d03f4ae115a1a9e3f2ff02e6e9d152
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2024-07-17 13:15:53 -0400

    new player sprites and hurt + death logic

--- scripts/characters/player/PlayerFSM.gd
+++ scripts/characters/player/PlayerFSM.gd
@@ -15,6 +15,9 @@ func _state_logic(_delta: float) -> void:
 	if state == states.idle or state == states.move:
 		parent.get_input()
 		parent.move()
+		
+	if not animation_player.is_playing():
+		animation_player.play("idle")
 	
 	
 func _get_transition() -> int:
@@ -26,15 +29,15 @@ func _get_transition() -> int:
 			if parent.velocity.length() < 10:
 				return states.idle
 		states.hurt:
-			if not animation_player.is_playing():
-				return states.idle
+			return states.idle
+		states.dead:
+			return states.dead
 	return -1
 	
 	
 func _enter_state(_previous_state: int, new_state: int) -> void:
 	match new_state:
-		states.idle:
-			animation_player.play("idle")
 		states.hurt:
 			animation_player.play("hurt")
-			parent.cancel_attack()
+		states.dead:
+			animation_player.play("dead")
